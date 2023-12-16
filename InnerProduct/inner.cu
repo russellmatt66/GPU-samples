a58350df229fa7bd6c71459cc49a46fc06503de5
Matt <russellmatt66@gmail.com>
2023-12-15 21:33:57 -0500

    Debugging illegal memory access in inner.cu

--- InnerProduct/inner.cu
+++ InnerProduct/inner.cu
@@ -1,26 +1,46 @@
+/*
+Kernel for benchmarking inner product computation using GeForce GTX 960 
+*/
+
 #include <stdio.h>
 #include <assert.h>
 #include <cuda_runtime.h>
+#include <curand.h>
+#include <curand_kernel.h>
+#include <math.h>
 
-// Mutex for inner product
-__device__ int mutex = 0;
-
-// Should have double, and int as well
-__global__ void InnerProduct(float *sum, float *a, float *b, int N){
+// Should have double, and 16-bit (?) kernels as well ("?" = if possible)
+__global__ void innerProduct(float *sum, float *a, float *b, int N){
     // Assume 1D execution configuration
     int threadNum = threadIdx.x + blockDim.x * blockIdx.x;
     int nthreads = blockDim.x * gridDim.x;
     int partial = 0;
 
     for (int it = threadNum; it < N; it += nthreads ){
-        partial = a[it] + b[it];
+        partial += a[it] + b[it];
     }
 
-    while (atomicExch(&mutex, 1) != 0){
-        *sum += partial; // This needs to be inside a mutex
-        atomicExch(&mutex,0);
+    atomicAdd(sum, partial);
+}
+
+// Fill out a and b with random values according to grid-stride method 
+__global__ void initRandom(float *a, float* b, int N){
+    int threadNum = threadIdx.x + blockDim.x * blockIdx.x; // tid or tnum is a better name for idiom
+    int nthreads = blockDim.x * gridDim.x;
+
+    /* Implement device random number generation with curand */
+    curandState_t state;
+    unsigned long long seed = 1234;
+    if (threadNum < N){
+        curand_init(seed, threadNum, 0, &state);
     }
 
+    for (int it = threadNum; it < N; it += nthreads){
+        float aRandomValue = static_cast<float>(curand_uniform(&state));
+        a[threadNum] = aRandomValue;
+        float bRandomValue = static_cast<float>(curand_uniform(&state));
+        b[threadNum] = bRandomValue;
+    }
 }
 
 // Error-checking Macro from NVIDIA DLI:GSAC CUDA C/C++ course
@@ -34,5 +54,30 @@ inline cudaError_t checkCuda(cudaError_t result)
 }
 
 int main(){
+    // Declare variables and allocate arrays
+    int N = 2<<20; // left-shifting 2 twenty-times gives 2^20
+    float *a, *b, *sum = 0;
 
+    int size = N * sizeof(float);
+    checkCuda(cudaMallocManaged(&a, size));
+    checkCuda(cudaMallocManaged(&b, size));
+
+    // Initialize vectors with random data
+    
+    /* Calculate execution configuration */
+    int num_blocks, num_threads_per_block; 
+    num_blocks = 1;
+    num_threads_per_block = 512;
+
+    initRandom<<<num_blocks, num_threads_per_block>>>(a, b, N);
+    checkCuda(cudaDeviceSynchronize());
+
+    // Call innerProduct Kernel 
+
+    innerProduct<<<num_blocks, num_threads_per_block>>>(sum, a, b, N);
+    checkCuda(cudaDeviceSynchronize());
+    
+    // Free arrays
+    checkCuda(cudaFree(a));
+    checkCuda(cudaFree(b));
 }
