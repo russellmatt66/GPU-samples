cc1ef2f4bf6fd0e32f21d8210a04aa4766905d46
Matt <russellmatt66@gmail.com>
2024-01-10 22:03:58 -0500

    machine-learning/clean.py finished, working on machine-learning/analyze.py now

--- BinarySearch/machine-learning/analyze.py
+++ BinarySearch/machine-learning/analyze.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Analyze the data in a general manner so that this code can be re-used with rtx 2060 data
 
 # Analysis targets:
-# (1a) Which run was the absolute fastest, i.e., lowest taukern?
+# (1a) Which configuration was the absolute fastest, i.e., lowest average taukern?
 # 
 # (1b) Which run achieved the absolute highest effective bandwidth?
 # - BW_{eff} = (4 * log2(Nx) * 4 * N) / (taukern * 10^-6) / 10^9
@@ -15,10 +15,25 @@ import pandas as pd
 # - (IV) To determine the effective bandwidth, divide by the walltime (taukern [ms]), and then divide by a factor of 10^9 to obtain how many GB/s of 
 #   data were transferred. 
 #
-# (2a) Which feature set had the best average performance?
+# (2a) Which feature set (num_blocks, threads_per) had the best average performance?
 # (2b) Which feature set had the best average bandwidth? 
 #
 # (3) Which feature set was the most consistent, i.e., lowest variance?
 #
 # Visualization targets:
 # (1) What does the performance landscape look like for a given problem size? 
+#   - Obtain this by calculating the effective bandwidth for every variable permutation
+
+def computeStatistics(data_csv: str) -> (int, int, list, list, list, list):
+    df = pd.read_csv(data_csv)
+    exec_config = [] 
+    avg_runtime = []
+    variance = []
+    eff_bandwidth = []
+    # Determine the problem size associated with the DataFrame
+    # - Use string methods to obtain N, Nx from data_csv
+    # Compute the average runtime, and variance, for each of the unique execution configurations
+    # Compute the (average) effective bandwidth for each of the unique execution configurations
+    return N, Nx, exec_config, avg_runtime, variance, eff_bandwidth
+
+# Need to get 
--- BinarySearch/machine-learning/clean.py
+++ BinarySearch/machine-learning/clean.py
@@ -1,23 +1,52 @@
 import pandas as pd
-
+import sys
+import os 
+import glob 
 '''
 Clean the dataset
 Here, that means the following:
 (1) The dataset is complete, i.e., no missing values, but for sufficiently large problems the CUDA timer malfunctioned, and reported times of either 0 ms 
     or ~10^{-41} ms. For what datasets this occurred must be determined, and they must be removed from the pool. 
 (2) Once the malformed datasets are determined, the problems for which they occurred will be recorded. 
-    - The ML component of this project will use this list as the basis for the test set
-(3) The remaining clean data will be used in the ML component as the training, and validation datasets.  
-
-Questions:
-(Q1): Should this module implement functions to clean kerneldata from a run on a device, or should it implement functionality to create a directory 
-   where the clean data, and list of malformed data, is stored so that it can be read in by analyze.py and randomforest.py?
-(A1): It should implement functionality, see project.png for flow.  
+    - The ML component of this project will use these problem sizes as the basis for the test set
+(3) The remaining clean data will be used in the ML component as the training, and validation datasets.
+    - The data analysis component will analyze the clean data in order to compute statistics
 '''
-# Program code
+# 
+def isDirty(data_csv: str) -> bool:
+    print(data_csv)
+    df = pd.read_csv(data_csv)
+    threshold = 1.0e-9
+    return df['taukern'].min() < threshold # malfunctioned data is either =0.0, or =1.0e-41 for this feature
 
-# Check that '*-kerneldata/' is taken as input
+# Program code
+kernel_data = sys.argv[1] # Should check that '*-kerneldata/' is taken as input, but who tf is being malicious with this
+print("kernel_data = {}".format(kernel_data))
+
+device_id = kernel_data.split('-')[0]
+print("device_id = {}".format(device_id))
 
 # Create output folder, '*-cleandata/', for the clean datasets, and list of malformed data 
+clean_dir = device_id + '-cleandata/'
+print("output location = {}".format(clean_dir))
 
-# Loop through all the datasets in '*-kerneldata/', and write the clean data to the output folder, and record what datasets are malformed
+# os.mkdir(clean_dir) # Add some exception handling to this
+try:
+    os.mkdir(clean_dir)
+    print(f"Directory '{clean_dir}' created successfully.")
+except FileExistsError:
+    print(f"Directory '{clean_dir}' already exists.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+# Loop through all the datasets in '*-kerneldata/'
+all_files = glob.glob(os.path.join(kernel_data, '**', '*'), recursive=True)
+
+# Filter out directories, leaving only files
+files_only = [file for file in all_files if (os.path.isfile(file) and (file != kernel_data + '/README.md'))]
+
+malformed_list = clean_dir + 'dirty.txt'
+with open(malformed_list, 'w') as dirty_data:
+    for file in files_only:
+        if isDirty(file):
+            dirty_data.write(file + '\n') # Want to record the problem size for which malfunction occurs
