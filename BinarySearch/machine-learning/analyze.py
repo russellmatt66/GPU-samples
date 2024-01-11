b36b047eae9dd432f5b43d8d573f5c3fe24622eb
Matt <russellmatt66@gmail.com>
2024-01-10 22:36:39 -0500

    Small update to code

--- BinarySearch/machine-learning/analyze.py
+++ BinarySearch/machine-learning/analyze.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import glob
+import os
+import sys
 
 # Analyze the data in a general manner so that this code can be re-used with rtx 2060 data
 
@@ -32,8 +35,36 @@ def computeStatistics(data_csv: str) -> (int, int, list, list, list, list):
     eff_bandwidth = []
     # Determine the problem size associated with the DataFrame
     # - Use string methods to obtain N, Nx from data_csv
+    parts = data_csv.split('.')[0] # get rid of 'csv' ending
+    parts = parts.split('/')
+    for part in parts: 
+        if part.startswith('N'):
+            N = int(part[1:])
+            break
+
+    other_part = data_csv.split('.')[0] # get rid of 'csv ending
+    other_part = other_part.split('_')[1]
+    Nx = other_part[2:]
+
+    # Determine the unique execution configurations (should be same for every dataset)
+    exec_config.append((df['num_blocks'].unique(),df['num_threads_per_block'].unique()))
     # Compute the average runtime, and variance, for each of the unique execution configurations
     # Compute the (average) effective bandwidth for each of the unique execution configurations
     return N, Nx, exec_config, avg_runtime, variance, eff_bandwidth
 
-# Need to get 
+# MAIN
+kernel_data = sys.argv[1]
+# Loop through all the datasets in '*-kerneldata/'
+all_files = glob.glob(os.path.join(kernel_data, '**', '*'), recursive=True)
+
+# Filter out directories, leaving only data files
+files_only = [file for file in all_files if (os.path.isfile(file) and (file != kernel_data + '/README.md'))]
+
+# Test computeStatistics
+(N, Nx, e_c, a_r, v, e_b) = computeStatistics(files_only[0])
+print(str(N) + '\n')
+print(str(Nx) + '\n')
+print(e_c)
+print(a_r)
+print(v)
+print(e_b)
